e646ceb2c8c963ee27a5523d3bebd8eb1185c23e
Raaghav-DC <106242981+Raaghav-DC@users.noreply.github.com>
2023-08-18 11:23:31 +0000

    Added 2 new Tables with Multiple Queries

---
+++ models/ExpandedlandingPage.sql
@@ -0,0 +1,6 @@
+
+{{ config(
+    materialized="view"
+) }}
+
+select * from dc_dc_frontendtest_003.gads_jump_expandedlandingpageperformance_new
---
+++ models/Vinculum.sql
@@ -0,0 +1,36 @@
+with SkuDetails as (
+
+    select * from dc_dc_frontendtest_003.vin_sku_details_kr_2
+
+),
+
+Inventory as (
+
+    select * from dc_dc_frontendtest_003.vin_inventory_status_kr
+
+),
+
+ReturnToVendor as (
+
+    select * from dc_dc_frontendtest_003.vin_return_to_vendor_details_kr_2_items 
+
+),
+
+
+finalTable as (
+
+    select
+        SkuDetails.skucode,  SkuDetails.skuname, SkuDetails.classification, SkuDetails.mrp, SkuDetails.saleprice, SkuDetails.basecost,
+        Inventory.skucode, Inventory.location, Inventory.qty
+        ReturnToVendor.skucode, ReturnToVendor.skuname, ReturnToVendor.basecost, ReturnToVendor.reason, ReturnToVendor.size
+
+    from SkuDetails 
+
+    full outer join 
+    on SkuDetails.skucode = Inventory.skucode
+
+    full outer join
+    on SkuDetails.skucode = ReturnToVendor
+)
+
+select * from finalTable
