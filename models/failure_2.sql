ff33275d069f072b71dc76055360dd97c9ae1d45
Raaghav-DC <raaghav.gaur@datachannel.co>
2026-01-19 13:59:22 +0530

    Self Sabotage 2 Models

--- models/failure_2.sql
+++ models/failure_2.sql
@@ -1,2 +1,2 @@
 create table `datachannel-237710.dc_dc_frontendtest_004.failure_2` as 
- SELECT * FROM `datachannel-237710.dc_dc_frontendtest_004.asc_betterbeing__get_ledger_summary_view_data`;
+ SELECT * FROM `datachannel-237710.dc_dc_frontendtest_004.asc_betterbeing__get_ledger_summary_view_data_7780`;
